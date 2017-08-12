fc1a9c246b3c05deb536227d64ba73c738389702
abonello <ab@anthonybonello.co.uk>
2017-08-12 23:25:01 +0100

    tidying script.js

--- js/script.js
+++ js/script.js
@@ -6,41 +6,5 @@ $(window).scroll (function() {
         //"transform" : "translate(-" + winScroll / 1  + "px, 0px)"
     });
 
-
-    console.log(winScroll);
 });
 
-
-
-
-// $(window).scroll(function() {
-
-//     var winScroll = $(this).scrollTop();
-
-//     $(".logo").css({
-//         "transform" : "translate(0, " + winScroll / 1.9 + "%)"
-//     });
-
-//     $(".backBird").css({
-//         "transform" : "translate(0," + winScroll / 3.5 + "%)"
-//     });
-
-//     $(".foreBird").css({
-//         "transform" : "translate(0, -" + winScroll / 90 + "%)"
-//     });
-
-//     // if((winScroll + ($(window).height()*2/3)) > $(".flexClothesPics").offset().top) {
-//     if((winScroll) > $(".flexClothesPics").offset().top - ($(window).height()*2/3)) {
-//         $(".flexClothesPics figure").each(function(i) {
-
-//             setTimeout(function() {
-//                 $(".flexClothesPics .anImage").eq(i).addClass("isShowing");
-//             }, 200 * (i+1));
-            
-//         });
-//     }
-//     console.log(winScroll);
-    
-
-
-// });
